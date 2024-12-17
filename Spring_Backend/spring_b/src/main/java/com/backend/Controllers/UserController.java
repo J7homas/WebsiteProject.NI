d40b0ac7ae1eefc5f0c92f6049ba1546242168a4
J7homas <J7homas@github.com>
2024-12-17 18:14:22 -0500

    Updated the UserController.

--- Spring_Backend/spring_b/src/main/java/com/backend/Controllers/UserController.java
+++ Spring_Backend/spring_b/src/main/java/com/backend/Controllers/UserController.java
@@ -4,11 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.backend.Models.UserModel;
 import com.backend.Repositories.InnerUserRepository;
@@ -18,65 +16,63 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-
-
-
-@Controller
+@RestController
 public class UserController {
+    
     @Autowired
     InnerUserRepository innerUserRepository;
-    UserService userService;
     
+    @Autowired
+    UserService userService;
+
     @GetMapping("/users")
-    public List<UserModel> getAllUsers(){
-        //repository.findAll()
+    public List<UserModel> getAllUsers() {
         return innerUserRepository.findAll();
     }
 
     @GetMapping("/users/id/{id}")
-    public UserModel getUserById(int id){
+    public ResponseEntity<UserModel> getUserById(@PathVariable Integer id) {
         Optional<UserModel> user = innerUserRepository.findById(id);
 
-        if(user.isEmpty()){
-            System.out.println("User isn't real.");
+        if (user.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
-        return user.get();
+        return new ResponseEntity<>(user.get(), HttpStatus.OK);
     }
 
     @PostMapping("/users")
-    public String postCreateUser(@RequestBody UserModel userModel){
+    public String postCreateUser(@RequestBody UserModel userModel) {
         innerUserRepository.save(userModel);
         return "User created.";
     }
-    
+
     @PutMapping("/users/id/{id}")
-    public ResponseEntity<UserModel> updateUser(@PathVariable("id") Integer id, UserModel userModel){
+    public ResponseEntity<UserModel> updateUser(@PathVariable("id") Integer id, @RequestBody UserModel userModel) {
         Optional<UserModel> userData = innerUserRepository.findById(id);
 
-        if(userData.isPresent()){
+        if (userData.isPresent()) {
             UserModel _user = userData.get();
 
-            _user.setFirst_Name(_user.getFirst_Name());
-            _user.setLast_Name(_user.getLast_Name());
+            _user.setFirst_Name(userModel.getFirst_Name());
+            _user.setLast_Name(userModel.getLast_Name());
 
-            return new ResponseEntity<>(innerUserRepository.save(_user), HttpStatusCode.valueOf(200));
+            return new ResponseEntity<>(innerUserRepository.save(_user), HttpStatus.OK);
         }
-        return new ResponseEntity<>(HttpStatusCode.valueOf(500));
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @DeleteMapping("/users/{id}")
-    public String deleteUserById(@PathVariable Integer id) {
+    public ResponseEntity<String> deleteUserById(@PathVariable Integer id) {
         userService.deleteUserById(id);
-
-        return "User has been deleted.";
+        return new ResponseEntity<>("User has been deleted.", HttpStatus.OK);
     }
 
     @DeleteMapping("/users")
-    public String deleteAllUsers() {
+    public ResponseEntity<String> deleteAllUsers() {
         userService.deleteAllUsers();
-
-        return "All users have been deleted.";
+        return new ResponseEntity<>("All users have been deleted.", HttpStatus.NO_CONTENT);
     }
 }
